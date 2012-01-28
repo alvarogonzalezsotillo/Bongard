3de339932077ce2512eafe636f8dbc67639b6909
Alvaro Gonzalez <alvaro.gonzalezsotillo@educa.madrid.org>
2012-01-28 16:38:57 +0100

    autosolve, for demo

--- src/purethought/gui/game/BGameField.java
+++ src/purethought/gui/game/BGameField.java
@@ -2,6 +2,7 @@ package purethought.gui.game;
 
 import purethought.animation.BAnimator;
 import purethought.animation.BCompoundTransformAnimation;
+import purethought.animation.BConcatenateAnimation;
 import purethought.animation.BRotateAnimation;
 import purethought.animation.BScaleAnimation;
 import purethought.animation.BTranslateAnimation;
@@ -29,9 +30,7 @@ import purethought.problem.BProblem;
 public class BGameField extends BDrawableContainer implements IBFlippableDrawable{
 	
 	private static final int FOCUS_DELAY = 100;
-
 	private static final double FOCUS_ZOOM = 1.3;
-
 	private static final boolean SHOW_POINTER = false;
 
 	private BProblem _problem;
@@ -43,23 +42,41 @@ public class BGameField extends BDrawableContainer implements IBFlippableDrawabl
 	private IBRectangle _size;
 	private BLabel _pointer = BFactory.instance().label("O");
 	
+	public void autoSolve(){
+		boolean isOfSet1 = _problem.isOfSet1(_questionSprite.raster());
+		
+		IBPoint orig = spritePosition(-1,1);
+		IBPoint dest = spritePosition(isOfSet1?0:1, 2);
+		dest = BFactory.instance().point((dest.x()*2+orig.x())/3, dest.y()+20);
+		
+		IBAnimation animation = createPickUpAnimation();
+		animation = new BConcatenateAnimation( animation,createDragAnimation(dest,900) );
+		animation = new BConcatenateAnimation( animation,createOverSetAnimation( isOfSet1?_set1Sprites:_set2Sprites) );
+		animation = new BConcatenateAnimation( animation,createDropAnimation(dest) );
+		animation = new BConcatenateAnimation( animation,createOutSetAnimation(isOfSet1?_set1Sprites:_set2Sprites, null) );
+		
+		BAnimator a = BFactory.instance().game().animator();
+		a.addAnimation(animation);
+	}
+
+	private IBAnimation _pickUpAnimation;
+	private IBAnimation _dropAnimation;
+	private IBAnimation _set1OverAnimation;
+	private IBAnimation _set2OverAnimation;
+	private BWaitForAnimation _set1DropAnimation;
+	private BWaitForAnimation _set2DropAnimation;
+
+	
 	private BEventAdapter _adapter = new BEventAdapter(this) {
 		
 		private boolean _dragQuestion = false;
-		private IBAnimation _pickUpAnimation;
-		private IBAnimation _dropAnimation;
-		private IBAnimation _dragAnimation;
-		private IBAnimation _set1OverAnimation;
 		private boolean _set1Over;
 		private boolean _set2Over;
-		private IBAnimation _set2OverAnimation;
-		private BWaitForAnimation _set1DropAnimation;
-		private BWaitForAnimation _set2DropAnimation;
 		
 		@Override
 		public boolean pointerDown(IBPoint p) {
 			if( _questionSprite.inside(p, null) ){
-				_pickUpAnimation = new BScaleAnimation(FOCUS_ZOOM, FOCUS_ZOOM, FOCUS_DELAY, _questionSprite);
+				_pickUpAnimation = createPickUpAnimation();
 				animator().addAnimation( _pickUpAnimation );
 				_dragQuestion = true;
 				return true;
@@ -70,35 +87,37 @@ public class BGameField extends BDrawableContainer implements IBFlippableDrawabl
 		@Override
 		public boolean pointerDrag(IBPoint p){
 			if( _dragQuestion ){
-				_dragAnimation = new BTranslateAnimation(p, 50, _questionSprite );
-				animator().addAnimation( new BWaitForAnimation(_dragAnimation, _pickUpAnimation) );
+				IBAnimation dragAnimation = createDragAnimation(p,50);
+				animator().addAnimation( dragAnimation );
 				checkSets();
 				return true;
 			}
 			return false;
 		}
 
+
 		private void checkSets() {
 			boolean set1Over = near( _questionSprite, _set1Sprites );
 			if( !_set1Over &&  set1Over ){
-				_set1OverAnimation = new BScaleAnimation( FOCUS_ZOOM, FOCUS_ZOOM, FOCUS_DELAY, _set1Sprites );
+				_set1OverAnimation = createOverSetAnimation(_set1Sprites);
 				animator().addAnimation(_set1OverAnimation);
 			}
 			if( _set1Over && !set1Over ){
-				animator().addAnimation( new BWaitForAnimation( new BScaleAnimation(1/FOCUS_ZOOM,1/FOCUS_ZOOM,FOCUS_DELAY,_set1Sprites), _set1OverAnimation) );
+				animator().addAnimation( createOutSetAnimation(_set1Sprites,_set1OverAnimation) );
 			}
 			_set1Over = set1Over;
 			
 			boolean set2Over = near( _questionSprite, _set2Sprites );
 			if( !_set2Over &&  set2Over ){
-				_set2OverAnimation = new BScaleAnimation( FOCUS_ZOOM, FOCUS_ZOOM, FOCUS_DELAY, _set2Sprites );
+				_set2OverAnimation = createOverSetAnimation(_set2Sprites);
 				animator().addAnimation(_set2OverAnimation);
 			}
 			if( _set2Over && !set2Over ){
-				animator().addAnimation( new BWaitForAnimation( new BScaleAnimation(1/FOCUS_ZOOM,1/FOCUS_ZOOM,FOCUS_DELAY,_set2Sprites ), _set2OverAnimation) );
+				animator().addAnimation( createOutSetAnimation(_set2Sprites, _set2OverAnimation) );
 			}
 			_set2Over = set2Over;
 		}
+
 
 		private boolean near(BSprite questionSprite, BSprite[] sprites) {
 			IBPoint p = questionSprite.temporaryPosition();
@@ -130,20 +149,16 @@ public class BGameField extends BDrawableContainer implements IBFlippableDrawabl
 					dest = _questionSprite.temporaryPosition();
 				}
 				
-				_dropAnimation = 
-						new BCompoundTransformAnimation(
-								new IBTransformAnimable[]{ _questionSprite }, 
-								new BScaleAnimation(1/FOCUS_ZOOM, 1/FOCUS_ZOOM, FOCUS_DELAY),
-								new BTranslateAnimation( dest, FOCUS_DELAY)
-						);
-				animator().addAnimation( new BWaitForAnimation(_dropAnimation, _pickUpAnimation) );
+				_dropAnimation = createDropAnimation(dest);
+				
+				animator().addAnimation( _dropAnimation );
 				
 				if( _set1Over ){
-					_set1DropAnimation = new BWaitForAnimation( new BScaleAnimation(1/FOCUS_ZOOM,1/FOCUS_ZOOM,FOCUS_DELAY,_set1Sprites ), _set1OverAnimation);
+					_set1DropAnimation = createOutSetAnimation(_set1Sprites,_set1OverAnimation);
 					animator().addAnimation( _set1DropAnimation );
 				}
 				if( _set2Over ){
-					_set2DropAnimation = new BWaitForAnimation( new BScaleAnimation(1/FOCUS_ZOOM,1/FOCUS_ZOOM,FOCUS_DELAY,_set2Sprites ), _set2OverAnimation);
+					_set2DropAnimation = createOutSetAnimation(_set2Sprites,_set2OverAnimation);
 					animator().addAnimation( _set2DropAnimation );
 				}
 				
@@ -231,32 +246,41 @@ public class BGameField extends BDrawableContainer implements IBFlippableDrawabl
 		alignSprites();
 	}
 	
+	private IBPoint spritePosition( int column, int row ){
+		int s = 105;
+		
+		if( column < 0 || row < 0 ){
+			return BFactory.instance().point(s*2,s*3);
+		}
+		
+		double colx = column == 0 ? .8 : 3.2;
+		
+		return BFactory.instance().point(s*colx, s*(row+1) );
+	}
+	
+	
 	private void alignSprites(){
 		int s = 105;
 		int millis = 400;
 		
-		BFactory f = BFactory.instance();
 		BAnimator animator = animator();
-		
-		
+
 		_size = new BRectangle(0, 0, s*4, s*6);
-		
-				
 		
 		for (int i = 0; i < _set1Sprites.length; i++) {
 			IBTransformAnimable a = _set1Sprites[i];
-			animator.addAnimation( new BTranslateAnimation( f.point(s*.8, s*(i+1) ), millis, a ) );
+			animator.addAnimation( new BTranslateAnimation( spritePosition(0,i), millis, a ) );
 		}
 
 		for (int i = 0; i < _set2Sprites.length; i++) {
 			IBTransformAnimable a = _set2Sprites[i];
-			animator.addAnimation( new BTranslateAnimation( f.point(s*3.2, s*(i+1) ), millis, a ) );
+			animator.addAnimation( new BTranslateAnimation( spritePosition(1,i), millis, a ) );
 		}
 
 		animator.addAnimation(
 			new BCompoundTransformAnimation(
 				new IBTransformAnimable[]{_questionSprite },
-				new BTranslateAnimation( f.point(s*2, s*3) , millis ),
+				new BTranslateAnimation( spritePosition(-1,-1) , millis ),
 				new BRotateAnimation(2*Math.PI/millis, millis)
 			)
 		);
@@ -322,6 +346,33 @@ public class BGameField extends BDrawableContainer implements IBFlippableDrawabl
 			return _badIcon;
 		}
 		return _icon;
+	}
+
+
+	private IBAnimation createPickUpAnimation() {
+		return new BScaleAnimation(FOCUS_ZOOM, FOCUS_ZOOM, FOCUS_DELAY, _questionSprite);
+	}
+
+	private IBAnimation createDragAnimation(IBPoint p, int millis) {
+		return new BWaitForAnimation(new BTranslateAnimation(p, millis, _questionSprite ), _pickUpAnimation);
+	}
+
+	private BWaitForAnimation createOutSetAnimation( BSprite[] setSprites, IBAnimation setOverAnimation) {
+		return new BWaitForAnimation( new BScaleAnimation(1/FOCUS_ZOOM,1/FOCUS_ZOOM,FOCUS_DELAY,setSprites), setOverAnimation);
+	}
+
+	private BScaleAnimation createOverSetAnimation( BSprite[] setSprites ) {
+		return new BScaleAnimation( FOCUS_ZOOM, FOCUS_ZOOM, FOCUS_DELAY, setSprites );
+	}
+
+	private BWaitForAnimation createDropAnimation(IBPoint dest) {
+		return new BWaitForAnimation( 
+				new BCompoundTransformAnimation(
+						new IBTransformAnimable[]{ _questionSprite }, 
+						new BScaleAnimation(1/FOCUS_ZOOM, 1/FOCUS_ZOOM, FOCUS_DELAY),
+						new BTranslateAnimation( dest, FOCUS_DELAY)
+				),
+				_pickUpAnimation );
 	}
 
 }
