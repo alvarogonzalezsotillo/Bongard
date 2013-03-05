ed670afc6f752a78e8fffc8fb8eac512823f8827
Alvaro Gonzalez <alvaro.gonzalezsotillo@educa.madrid.org>
2013-03-05 10:45:36 +0100

    Added a default handler for uncaugth exceptions

--- ollitos-andr/src/ollitos/platform/andr/AndrActivity.java
+++ ollitos-andr/src/ollitos/platform/andr/AndrActivity.java
@@ -1,11 +1,13 @@
 package ollitos.platform.andr;
 
+import java.lang.Thread.UncaughtExceptionHandler;
+
 import ollitos.platform.BPlatform;
 import ollitos.platform.IBGame;
-import ollitos.platform.state.BState;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
@@ -14,10 +16,26 @@ import bongard.gui.game.BStartField;
 
 public class AndrActivity extends Activity {
 
+	/**
+	http://stackoverflow.com/questions/11225209/thread-exiting-with-uncaught-exception-no-stack-trace
+	*/
+	private void modifyUncaunghtExceptionHandler(){
+		final UncaughtExceptionHandler UEH = Thread.currentThread().getUncaughtExceptionHandler();
+		Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+		    @Override
+		    public void uncaughtException(Thread paramThread, Throwable paramThrowable) {
+			    Log.getStackTraceString(paramThrowable);
+			    Log.e("UncaughtE", "uncaughtException", paramThrowable);
+			    UEH.uncaughtException(paramThread, paramThrowable);
+		    }
+		});
+	}
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		modifyUncaunghtExceptionHandler();
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
